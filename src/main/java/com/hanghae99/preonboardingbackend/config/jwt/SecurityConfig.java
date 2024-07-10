8fa59637e0506fef2de04fca250b77cce148b80f
codingjang <jjangga813@naver.com>
2024-07-10 11:21:27 +0900

    feat : h2 console 사용 위한 SecurityConfig 설정

--- src/main/java/com/hanghae99/preonboardingbackend/config/jwt/SecurityConfig.java
+++ src/main/java/com/hanghae99/preonboardingbackend/config/jwt/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.hanghae99.preonboardingbackend.config.jwt;
 
+import com.hanghae99.preonboardingbackend.exception.jwt.JwtAccessDeniedHandler;
+import com.hanghae99.preonboardingbackend.exception.jwt.JwtAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
@@ -18,10 +21,34 @@ import org.springframework.security.web.SecurityFilterChain;
 
 
 public class SecurityConfig {
-    @Bean
+
+    private final JwtAuthenticationEntryPoint authenticationEntryPoint;
+    private final JwtAccessDeniedHandler accessDeniedHandler;
+
+    @Bean                     //HttpSecurity 객체를 통해 보안 설정을 정의
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().requestMatchers("/**").permitAll()
-                .anyRequest().authenticated();
+        http
+                .httpBasic(httpBasic -> httpBasic.disable())    //httpBasic과 formLogin 필터를 비활성화
+                .formLogin(formLogin -> formLogin.disable())
+                .csrf(csrf -> csrf.disable())                   //H2 콘솔을 사용하기 위해 CSRF 보호를 비활성화
+                .headers(headers -> headers                     //iframe 내에서 H2 콘솔이 제대로 작동하도록
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                .authorizeHttpRequests(authorize -> authorize   //요청에 대한 보안 규칙을 설정
+                        .requestMatchers(
+                                "/logIn",
+                                "/signUp",
+                                "/swagger-ui/**",
+                                "v3/api-docs/**",
+                                "/h2-console/**",
+                                "/error"
+                        ).permitAll()                           //경로에 대한 모든 요청을 허용
+                        .anyRequest().authenticated()           //다른 모든 요청은 인증을 요구
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling   // 에러 처리 핸들러 설정
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                        .accessDeniedHandler(accessDeniedHandler)
+                );
+
         return http.build();
     }
 }
